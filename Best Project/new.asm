5789179f8884e11b084a4457772d4071706ecf43
MuhammadMustafa12 <mustafatariq365@protonmail.com>
2024-11-12 15:44:09 +0500

    Part 1, NO VALIDATION OR IF ELSES HERE

--- Best Project/new.asm
+++ Best Project/new.asm
@@ -1,21 +1,63 @@
 include irvine32.inc
 .data
 dot byte ".", 0
-space byte "  ", 0
+
+space byte "   ", 0
+dspace byte "  ", 0
 singlespace byte " ", 0
-
-r0 byte ".  .  .  .  .", 0
-r1 byte "                  .", 0
-r2 byte ".  .  .  .  .", 0
-r3 byte "                  .", 0
-r4 byte ".  .  .  .  .", 0
-r5 byte "                  .", 0
-r6 byte ".  .  .  .  .", 0
-r7 byte "                  .", 0
-r8 byte ".  .  .  .  .", 0
+substr1 BYTE 2 DUP(0)       ; Reserve space for one character + null terminator
+tempeax dword ?
+tempecx dword ?
 
 
-array dword offset r0, offset r1, offset r2, offset r3, offset r4, offset r5, offset r6, offset r7, offset r8
+r0 byte ".   .   .   .   .", 0
+r1 byte "                       .", 0
+r2 byte "                       .", 0
+r3 byte ".   .   .   .   .", 0
+r4 byte "                       .", 0
+r5 byte "                       .", 0
+r6 byte ".   .   .   .   .", 0
+r7 byte "                       .", 0
+r8 byte "                       .", 0
+r9 byte ".   .   .   .   .", 0
+r10 byte "                       .", 0
+r11 byte "                       .", 0
+r12 byte ".   .   .   .   .", 0
+
+
+r13 byte "                 ", 0
+r14 byte "                       .", 0
+r15 byte "                       .", 0
+r16 byte "                 ", 0
+r17 byte "                       .", 0
+r18 byte "                       .", 0
+r19 byte "                 ", 0
+r20 byte "                       .", 0
+r21 byte "                       .", 0
+r22 byte "                 ", 0
+r23 byte "                       .", 0
+r24 byte "                       .", 0
+r25 byte "                 ", 0
+
+r26 byte "                 ", 0
+r27 byte "                       .", 0
+r28 byte "                       .", 0
+r29 byte "                 ", 0
+r30 byte "                       .", 0
+r31 byte "                       .", 0
+r32 byte "                 ", 0
+r33 byte "                       .", 0
+r34 byte "                       .", 0
+r35 byte "                 ", 0
+r36 byte "                       .", 0
+r37 byte "                       .", 0
+r38 byte "                 ", 0
+
+
+array dword offset r0, offset r1, offset r2, offset r3, offset r4, offset r5, offset r6, offset r7, offset r8, offset r9, offset r10, offset r11, offset r12
+RedArray dword offset r13, offset r14, offset r15, offset r16, offset r17, offset r18, offset r19, offset r20, offset r21, offset r22, offset r23, offset r24, offset r25
+BlueArray dword offset r26, offset r27, offset r28, offset r29, offset r30, offset r31, offset r32, offset r33, offset r34, offset r35, offset r36, offset r37, offset r38
+
 
 startc byte ?
 startr byte ?
@@ -23,14 +65,38 @@ startr byte ?
 startPrompt byte "Input the Row: ", 0
 endPrompt byte "Input the Col: ", 0
 rcPrompt byte "Input the row or column movement: ", 0
+p1prompt byte "Player 1", 0
+p2prompt byte "Player 2", 0
+contprompt byte "Press any key to continue: ", 0
 
+turns dword 0
 rc byte ?
 row byte "r", 0
 .code
 main proc
 	
-	
+	mov eax, 0
 	gameLoop:
+	call drawgridbox
+	mov eax, turns
+	test turns, 1
+	jz player1    ;player 1 turn
+	
+	player2:
+	mov edx, offset p2prompt
+	call writestring
+	call crlf
+	jmp cont
+	
+	player1:
+	mov edx, offset p1prompt
+	call writestring
+	call crlf
+
+
+
+
+	cont:
     call takeInputs
     
     mov esi, 0
@@ -50,6 +116,11 @@ stringsAreNotEqual:
 
 continueGame:
     call drawgridbox
+	inc turns
+	mov edx, offset contprompt
+	call writestring
+	call ReadChar  ; wait for key press
+	call clrscr
     jmp gameLoop ; repeat the game loop
 
 
@@ -92,21 +163,63 @@ takeInputs proc
 	takeInputs endp
 
 modifyRow proc
-    ; Get the row address
-    movzx esi, startr           ; Zero-extend startr to esi (row index)
-	imul esi, 2
-    mov eax, array[esi * type array] ; Load the address of the specified row
+    ; For main array
+    movzx esi, startr           
+	imul esi, 3
+    mov eax, array[esi * type array] 
 
-    ; Calculate the offset for the column in the row
-    movzx ecx, startc                ; Zero-extend startc to ecx (column index)
+    movzx ecx, startc                
 	inc eax
-    imul ecx, 3                      ; Each dot and space take 3 bytes (".  ")
-    add eax, ecx                     ; Move to the desired column
+    imul ecx, 4                      
+    add eax, ecx                     
 
-    ; Modify the character at the position to '_ _'
-    mov byte ptr [eax], '_'          ; Replace '.' with '_'
+    mov byte ptr [eax], '_'          
     inc eax
-    mov byte ptr [eax], '_'          ; Replace the next character with '_'
+    mov byte ptr [eax], '_'          
+	inc eax
+	mov byte ptr [eax], '_' 
+
+	; for red or blue array
+
+	test turns, 1	; if even(0) player 1, else player 2
+	jz p1
+	; p2 part, Bluearray
+	
+	movzx esi, startr           
+	imul esi, 3
+    mov eax, Bluearray[esi * type array] 
+
+    movzx ecx, startc                
+	inc eax
+    imul ecx, 4                      
+    add eax, ecx                     
+
+    mov byte ptr [eax], '_'          
+    inc eax
+    mov byte ptr [eax], '_'          
+	inc eax
+	mov byte ptr [eax], '_'
+	jmp cont
+
+	; p1 part, Redarray
+	p1:
+	
+	movzx esi, startr           
+	imul esi, 3
+    mov eax, Redarray[esi * type array] 
+
+    movzx ecx, startc                
+	inc eax
+    imul ecx, 4                      
+    add eax, ecx                     
+
+    mov byte ptr [eax], '_'          
+    inc eax
+    mov byte ptr [eax], '_'          
+	inc eax
+	mov byte ptr [eax], '_'
+
+	cont:
     ret
 modifyRow endp
 
@@ -114,19 +227,74 @@ modifyRow endp
 
 
 modifyCol proc
+	
 	movzx esi, startr
-	imul esi, 2
+	imul esi, 3
 	inc esi 
 	
     
 	mov eax, array[esi * TYPE array] 
 	
     movzx ecx, startc
-	imul ecx, 3
+	imul ecx, 4
 	add eax, ecx
 
     mov byte ptr [eax], '|' 
+	inc esi
+	mov eax, array[esi * TYPE array] 
+	movzx ecx, startc
+	imul ecx, 4
+	add eax, ecx
+	mov byte ptr [eax], '|' 
+
+
+	test turns, 1	; if even(0) player 1, else player 2
+	jz p1
+	; p2 part, Bluearray
 	
+	movzx esi, startr
+	imul esi, 3
+	inc esi 
+	
+    
+	mov eax, Bluearray[esi * TYPE array] 
+	
+    movzx ecx, startc
+	imul ecx, 4
+	add eax, ecx
+
+    mov byte ptr [eax], '|' 
+	inc esi
+	mov eax, Bluearray[esi * TYPE array] 
+	movzx ecx, startc
+	imul ecx, 4
+	add eax, ecx
+	mov byte ptr [eax], '|' 
+	jmp cont
+
+
+	p1:
+	movzx esi, startr
+	imul esi, 3
+	inc esi 
+	
+    
+	mov eax, Redarray[esi * TYPE array] 
+	
+    movzx ecx, startc
+	imul ecx, 4
+	add eax, ecx
+
+    mov byte ptr [eax], '|' 
+	inc esi
+	mov eax, Redarray[esi * TYPE array] 
+	movzx ecx, startc
+	imul ecx, 4
+	add eax, ecx
+	mov byte ptr [eax], '|' 
+
+
+	cont:
     ret
 modifyCol endp
 
@@ -136,17 +304,20 @@ modifyCol endp
 
 
 drawgridbox proc	
+	
+	mov eax, white
+	call settextcolor
 	mov eax, 0
-	mov edx, offset space
+	mov edx, offset dspace
 	call writestring
-	l2:
+	l3:
 		call writedec
 		mov edx, offset space
 		call writestring
 		cmp eax, 4
 		jge out3
 		inc eax
-		jmp l2
+		jmp l3
 	
 	out3:
 	mov esi, 0
@@ -155,32 +326,88 @@ drawgridbox proc
 	call crlf
 	mov ecx, 0
 	
-	l1:
-		cmp eax, 9
-		jge out1
-		
-		test eax, 1 ; when line is even ie 0 2 4, zero comes and we print the column number
+
+	L2:
+		Cmp eax, 13
+		Jge out2
+
+		mov tempeax, eax
+		mov tempecx, ecx
+		mov eax, white
+		call settextcolor
+		mov eax, tempeax
+		mov ecx, 3
+		mov edx, 0 ; 
+		div ecx	; eax gets divided by 3, edx has the remainder, 
+		test edx, edx
 		jnz nn
-		push eax
-		mov eax, ecx
-		call writedec
-		pop eax
-		inc ecx
-		jmp cont
-		nn:
-		mov edx, offset singlespace
-		call writestring
-		cont:
-		mov edx, offset singlespace
-		call writestring
-		mov edx, array[esi* type array]
-		call writestring
-		call crlf
+		call writedec ; quotient already in eax
 		
-		inc esi
-		inc eax
-		jmp l1
-	out1:
+		jmp cont1
+		
+		nn:
+			mov edx, offset singlespace
+			call writestring
+		cont1:
+		mov edx, offset singlespace
+		call writestring
+		mov eax, tempeax
+		mov ecx, tempecx
+
+		mov ecx, array[esi * type array]		; current string
+		mov edx, Redarray[esi * type Redarray]
+		mov edi, Bluearray[esi * type Bluearray]
+		mov tempeax, eax
+		
+		L1:
+			Cmp byte ptr [ecx], 0	; check if end of current
+			Je out1
+			Mov bl, [ecx]		; only one byte gets moved
+			Mov bh, [edx]		; the red array
+			Mov [substr1], bl
+			mov byte ptr [substr1 + 1], 0
+			push edx
+			Cmp bl, bh			
+			Je dosmth
+	
+			Dosmthelse:
+				Mov bl, [ecx]		; only one byte gets moved
+				Mov bh, [edi]		; the red array
+				Cmp bl, bh
+				jne cont3
+				mov eax, blue
+				call settextcolor
+				jmp cont
+				cont3:
+				Mov eax, white
+				Call settextcolor
+				Jmp cont
+			Dosmth:
+	
+				Mov eax, red
+				Call settextcolor
+			Cont:
+				Mov edx, offset substr1
+				Call writestring
+				Pop edx
+				Inc edx
+				Inc ecx
+				inc edi
+				Jmp l1
+				Out1:
+
+			mov eax, tempeax
+			Inc eax	; eax is used as row counter
+			Inc esi	; row index
+			Call crlf
+			Jmp l2
+			Out2:
+
+	
+	call crlf
+	mov eax, white
+	call settextcolor
 	ret
 drawgridbox endp
+
 end main
